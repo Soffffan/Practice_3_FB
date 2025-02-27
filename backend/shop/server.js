44f82afaf4cfe3d2ea0a7932e9353f4309ce87db
Soffffan <baranova.sof@yandex.ru>
2025-02-27 18:33:17 +0300

    чето починилось

--- backend/shop/server.js
+++ backend/shop/server.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');  
 const app = express();
 const port = 3000;
 
-app.use(express.static('frontend/shop'));
+app.use(express.static(path.join(__dirname, 'index.html')));
 
 app.get('/api/products', (req, res) => {
-    fs.readFile('backend/data/products.json', (err, data) => {
+    fs.readFile(path.join(__dirname, 'products.json'), (err, data) => {
         if (err) {
             res.status(500).send('Error reading products file');
         } else {
             res.json(JSON.parse(data));
         }
     });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
